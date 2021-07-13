f8235e107d92dccbf2ebbe53c8398ebc3e95ea34
Dna-78 <natarius78@gmail.com>
2021-07-13 21:14:16 +0300

    Massive

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,15 +1,35 @@
 ﻿#include <iostream>
-#include "Employer.h"
-using namespace std;
+#include "Massive.h"
+#include "Massive.cpp"
+
+//#include "Employer.h"
+//using namespace std;
+//
+//int main()
+//{
+//	Employer** employers = new Employer * [3];
+//	employers[0] = new President("John", 52);
+//	employers[1] = new Manager("Garry", 46);
+//	employers[2] = new Worker("Ted", 38);
+//	for (int i = 0; i < 3; i++)
+//	{
+//		employers[i]->Print();
+//	}
+//}
 
 int main()
 {
-	Employer** employers = new Employer * [3];
-	employers[0] = new President("John", 52);
-	employers[1] = new Manager("Garry", 46);
-	employers[2] = new Worker("Ted", 38);
-	for (int i = 0; i < 3; i++)
+	try
 	{
-		employers[i]->Print();
+		ArrayTemp<int> myMass(5);
+		cout << myMass << endl;
+		myMass.Insert(1, 5);
+		cout << myMass << endl;
+		myMass.Remove(3);
+		cout << myMass << endl;
+	}
+	catch (exception& exp)
+	{
+		cout << exp.what() << endl;
 	}
 }
---
+++ ConsoleApplication1/ConsoleApplication1/Massive.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "Massive.h"
+#include "Massive.h"
+
+template<typename T>
+ArrayTemp<T>::ArrayTemp(int size)
+{
+	try
+	{
+		if (size < 0 || size > 1000000000)
+			throw invalid_argument("bad argument size: should be >= 0 and < 1000000000");
+
+		this->mass = new T[size];
+		this->size = size;
+		for (int i = 0; i < this->size; i++)
+		{
+			this->mass[i] = i + 1;
+		}
+	}
+	catch (exception& exp)
+	{
+		cout << exp.what() << endl;
+	}
+}
+
+template<typename T>
+void ArrayTemp<T>::Insert(T value, int index)
+{
+	try
+	{
+		if (index < 0 || index > this->size)
+			throw out_of_range("index < 0  or > size of mass");
+			T * helpMass = new T[this->size + 1];
+			int i = 0;
+	    for (; i < index; i++)
+	    {
+			helpMass[i] = this->mass[i];
+			 
+	    }
+		helpMass[i++] = value;
+		for (; i < this->size; i++)
+			helpMass[i] = this->mass[i];
+
+		delete[] this->mass;
+		this->mass = helpMass;
+		this->size++;
+	}
+	catch (exception& exp)
+	{
+		cout << "Insert error" << exp.what() << endl;
+	}
+}
+
+template<typename T>
+T ArrayTemp<T>::Remove(int index)
+{
+	try
+	{
+		if (index < 0 || index > this->size)
+			throw out_of_range("index < 0  or > size of mass");
+		T* helpMass = new T[this->size - 1];
+		int i = 0;
+		for (; i < index; i++)
+			helpMass[i] = this->mass[i];
+		for (; i < this->size - 1; i++)
+			helpMass[i] = this->mass[i + 1];
+
+		T help = this->mass[index];
+		delete[] this->mass;
+		this->mass = helpMass;
+		this->size--;
+		return help;
+	}
+	catch (exception& exp)
+	{
+		cout << "Remove error" << exp.what() << endl;
+		throw;
+	}	
+}
